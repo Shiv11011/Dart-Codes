b5557e79566597b7c8bfb2018fff58c44be59b36
Shivam Bhansali <Shivbhansali1011@gmail.com>
2024-08-18 17:00:57 +0530

    Assignment7 nested For

---
+++ Assignment7/Program1.dart
@@ -0,0 +1,14 @@
+/* *#  *#  *#
+   *#  *#  *#
+   *#  *#  *# */
+
+   import 'dart:io';
+   void main(){
+    int rows =int.parse(stdin.readLineSync()!);
+    for(int i=0;i<rows;i++){
+      for(int j=0;j<rows;j++){
+        stdout.write("*#\t");
+      }
+      print(" ");
+    }
+   }
